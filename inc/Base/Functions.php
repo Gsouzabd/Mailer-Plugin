2044ddffabb79ffb7051fcc5f35ade6e83bbe236
gabrielsouza <gabrielsouzabd@outlook.com>
2023-03-09 17:59:49 -0300

    functions updated

--- inc/Base/Functions.php
+++ inc/Base/Functions.php
@@ -12,14 +12,15 @@ class Functions{
     }
 
     function order_processing($order_id) {
+
         $urlSite = get_site_url();
         $nameSite = get_bloginfo( 'name' );
         $adminEmail = WC()->mailer()->get_emails()['WC_Email_New_Order']->recipient;
         
-        if( is_woocommerce() || is_cart() || is_shop() || is_checkout() ) return;
-
         $sender = new Sender();
         $sender->send($order_id,$urlSite, $nameSite, $adminEmail);
+		
+		if( is_woocommerce() || is_cart() || is_shop() || is_checkout() ) return;
 
         wp_redirect('edit.php?post_type=shop_order');
         exit;
